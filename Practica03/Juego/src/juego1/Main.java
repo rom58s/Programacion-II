1d3016401ad0540506c13c8cc28327c0359e2f44
Romulo Mateo Mamani Challapa <mateotroll41@gmail.com>
2026-03-31 15:27:51 -0400

    Add files via upload

---
+++ Practica03/Juego/src/juego1/Main.java
@@ -0,0 +1,15 @@
+package juego1;
+
+class Main {
+
+	public static void main(String[] args) {
+		/*tras crear una instancia de la 
+		  nueva clase  JuegoAdivinaNumero que se ha creado, llama al 
+		  metodo juega.*/
+		
+		JuegoAdivinaNumero j1 = new JuegoAdivinaNumero(100);
+		j1.juega();
+
+	}
+
+}
